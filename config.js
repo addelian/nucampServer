b2daa6a0b4090c9e2bae81e3dfe40ec52e31c66f
Nic Addelia <addelia.nicolas@gmail.com>
2020-06-15 20:36:15 -0500

    Passport Facebook

--- config.js
+++ config.js
@@ -1,4 +1,8 @@
 module.exports = {
     'secretKey': '12345-67890-09876-54321',
-    'mongoUrl': 'mongodb://localhost:27017/nucampsite'
+    'mongoUrl': 'mongodb://localhost:27017/nucampsite',
+    'facebook': {
+        clientId: '192512542067334',
+        clientSecret: '73ed7f91cd5aa01f26920224ac9967cc'
+    }
 }
